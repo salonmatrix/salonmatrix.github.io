a5eeaa44fa3cb9e729e1174ce2e8e1da82c9baf0
f-plus <slnmtrx@gmail.com>
2015-10-17 17:25:31 +0200

    Extend schema validation

--- test/valid-data.js
+++ test/valid-data.js
@@ -1,33 +1,80 @@
 var assert = require('assert');
 var glob = require('glob');
+var iso3166 = require('iso-3166-2');
 var matter = require('gray-matter');
 var path = require('path');
 var schema = require('node-verifier-schema');
 var util = require('util');
+var validator = require('validator');
+
+var Rule = schema.Verifier.Rule;
+
+Rule.add('country', Rule.extend({
+    check: function (value, params, done) {
+        if (typeof value === 'string' && 'name' in iso3166.country(value)) {
+            done(null, true);
+        } else {
+            done(new Error('Invalid country code.'), false);
+        }
+    }
+}));
+
+Rule.add('url', Rule.extend({
+    check: function (value, params, done) {
+        if (typeof value === 'string' && validator.isURL(value, { protocols: ['http', 'https'] })) {
+            done(null, true);
+        } else {
+            done(new Error('Invalid URL.'), false);
+        }
+    }
+}));
+
+Rule.add('nullable_boolean', Rule.extend({
+    check: function (value, params, done) {
+        if (value === null || value === true || value === false) {
+            done(null, true);
+        } else {
+            done(new Error('Invalid boolean value.'), false);
+        }
+    }
+}));
+
 
 var verifier = schema.create().object(function () {
-  this.field('name');
-  this.field('website');
-  this.field('vendor');
-  this.field('vendor_website').optional();
-  this.field('features').object(function () {
-    this.field('api').optional();
-    this.field('clients').optional();
-    this.field('employees').optional();
-    this.field('inventory').optional();
-    this.field('multi_location').optional();
-    this.field('multi_user').optional();
-    this.field('online_booking').optional();
-    this.field('reminders').optional();
-    this.field('resources').optional();
-    this.field('scheduling').optional();
-    this.field('payroll').optional();
-    this.field('pos').optional();
+  this.required('name', ['type string']);
+  this.required('website', ['url']);
+  this.required('vendor').object(function () {
+    this.optional('country', ['country']);
+    this.optional('employees', ['type number']);
+    this.optional('founded', ['type number']);
+    this.optional('name', ['type string']);
+    this.optional('website', ['url']);
   });
-  this.field('deployment').object(function () {
-    this.field('web').optional();
-    this.field('installed').optional();
+  this.required('features').object(function () {
+    this.optional('api', ['nullable_boolean']);
+    this.optional('booth_rental', ['nullable_boolean']);
+    this.optional('clients', ['nullable_boolean']);
+    this.optional('marketing', ['nullable_boolean']);
+    this.optional('employees', ['nullable_boolean']);
+    this.optional('inventory', ['nullable_boolean']);
+    this.optional('loyalty_program', ['nullable_boolean']);
+    this.optional('multi_location', ['nullable_boolean']);
+    this.optional('multi_user', ['nullable_boolean']);
+    this.optional('online_booking', ['nullable_boolean']);
+    this.optional('recurring', ['nullable_boolean']);
+    this.optional('reminders', ['nullable_boolean']);
+    this.optional('resources', ['nullable_boolean']);
+    this.optional('scheduling', ['nullable_boolean']);
+    this.optional('payroll', ['nullable_boolean']);
+    this.optional('pos', ['nullable_boolean']);
   });
+  this.required('deployment').object(function () {
+    this.optional('web', ['nullable_boolean']);
+    this.optional('mobile', ['nullable_boolean']);
+    this.optional('installed', ['nullable_boolean']);
+  });
+  this.optional('free_trial', ['nullable_boolean']);
+  this.optional('pricing', ['type string']);
 }).verifier();
 
 var files = glob.sync('../data/*.md', { cwd: __dirname, realpath: true });
